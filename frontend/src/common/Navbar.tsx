318e5258be8673b589493d4c6b0ef2548d41e68e
Maria Chirnogeanu <maria.chirnogeanu@stud.acs.upb.ro>
2025-04-05 23:17:27 +0300

    Modified nav bar

--- frontend/src/common/Navbar.tsx
+++ frontend/src/common/Navbar.tsx
@@ -1,5 +1,43 @@
 export default function Navbar() {
-    return <nav className="w-full bg-gray-800 text-white flex justify-between items-center p-4">
-
+    return <nav className="w-full bg-gray-800 flex justify-between items-center p-4">
+        <div className="w-full flex flex-row flex-wrap p-2 items-start gap-4">
+            <div className="flex items-center flex-col bg-amber-200 box-content
+                    rounded-lg px-2"
+                onClick={() => { }}
+            >
+                <img src={"/symbols/mulberry/emergency.png"} alt="Send" className="w-[20vw] max-w-[250px] h-[15vh] object-contain" />
+                <div>EMERGENCY</div>
+            </div>
+        </div>
+        <div className="w-full flex flex-row flex-wrap p-2 items-start gap-4">
+            <div className="flex items-center flex-col bg-blue-200 box-content
+                    rounded-lg px-2"
+                onClick={() => { }}
+            >
+                <img src={"/symbols/mulberry/iphone.svg"} alt="Send" className="w-[20vw] max-w-[250px] h-[15vh] object-contain" />
+                <div>Phone Call</div>
+            </div>
+        </div>
+        <div className="w-full flex flex-row flex-wrap p-2 items-start gap-4">
+            <div className="flex items-center flex-col bg-white box-content
+                    rounded-lg px-2"
+                onClick={() => { }}
+            >
+                <img src={"/symbols/mulberry/message_bubble.png"} alt="Send" className="w-[20vw] max-w-[250px] h-[15vh] object-contain" />
+                <div>Message</div>
+            </div>
+        </div>
     </nav>
 }
+
+// export default function ImageButton({ onClick }: { onClick: () => void }) {
+//     return (
+//       <button
+//         onClick={onClick}
+//         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-flex items-center"
+//       >
+//         <img src={"/symbols/mulberry/emergency.png"} alt="Send" className="w-6 h-6 mr-2" />
+//         Send Message
+//       </button>
+//     );
+//   }
